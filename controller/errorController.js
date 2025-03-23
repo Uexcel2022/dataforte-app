e6301938c00b44f7e8d173bb1eaf6557f7de1c32
Uexcel2022 <excellenceudoka@gmail.com>
2025-03-23 02:00:07 +0100

    update

--- controller/errorController.js
+++ controller/errorController.js
@@ -4,11 +4,6 @@ function errorHandler(error,req,resp,next){
     error.statusCode = error.statusCode||500
     error.status = error.status||'error'
 
-    // if(error.name=='ValidationError'){
-    //   const msg =  Object.values(error.details)
-    //   .map(el=>el.message.endsWith('[ref:password]')?'Password are not the same.':el.message);
-    //   error.message = msg;
-    // }
 
     if(`${error.name}`.startsWith('ValidationError')){
       const msg =  Object.values(error.errors).map(el=>el.message).join(', ')
