f03a8ee592e74ec10b4a3fc7d65048f4df7be297
Uexcel2022 <excellenceudoka@gmail.com>
2025-04-18 01:46:21 +0100

    update

--- controller/authController.js
+++ controller/authController.js
@@ -69,9 +69,6 @@ const login = catchAsync(async(req,res, next)=>{
    const cookieOptions = {
         expires: new Date(Date.now()+process.env.COOKIE_EXPIRES_IN*24*60*60*1000),
         httpOnly: true,
-        sameSite: 'None',
-        partitioned: true,
-        path: '/'
     }
   
     if(process.env.NODE_ENV.match(/^production$/)){
